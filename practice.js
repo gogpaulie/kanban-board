4ef5c53e919d734dd1c63de017f64ace5eb5b813
GoG_Paulie <ppolih21@gmail.com>
2020-09-15 08:49:16 -0400

    first commit

---
+++ practice.js
@@ -0,0 +1,7 @@
+const array1 = ['a', 'b', 'c', 'x'];
+const array2 = ['z', 'y', 'i'];
+const array3 = ['z', 'y', 'x'];
+
+function arrayContainsMatch(arr1, arr2) {
+  for (let i = 0; i < arr; i++) {}
+}
